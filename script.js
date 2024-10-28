0807ace43309170e932d3a948a79df0cbf85aa89
Diego Oliveira <diegosesi7722@gmail.com>
2024-10-28 08:45:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    const slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex - 1].style.display = "block";  
+    setTimeout(showSlides, 3000); // Muda a imagem a cada 3 segundos
+}
+
+function plusSlides(n) {
+    slideIndex += n - 1; // Ajusta o índice
+    showSlides();
+}
